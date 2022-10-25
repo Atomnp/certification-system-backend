0e64e61674c17fa90a463c48f6c9e7febf43689f
aayush <neupane0403@gmail.com>
2022-10-25 13:36:50 +0545

    Access token valid for 24 hours

--- certification_system/certification_system/settings.py
+++ certification_system/certification_system/settings.py
@@ -178,7 +178,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "authencation.UserAccount"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=10),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
